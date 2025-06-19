192f02c7eafc029d02cfe19e18f2f6b7377c9bfa
sureshjoshi88 <sj3795789@gmail.com>
2025-06-19 15:02:29 +0530

    updated

--- test-for-react/src/component/Navbar.jsx
+++ test-for-react/src/component/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { IoIosMoon } from "react-icons/io";
 import { IoIosSunny } from "react-icons/io";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = (props) => {
   return (
     <div className={`w-full sticky top-0 z-40 ${props.mode==='light'?'bg-white':'bg-black'}`}>
        <div className='flex justify-between p-3'>
         <p className='font-medium text-lg'>Movies</p>
-        <Link className='font-medium text-lg' to='/' >Home</Link>
-        <Link className='font-medium text-lg' to='/watchlist' >WatchList</Link>
-        <Link className='font-medium text-lg' to='/login' >Login</Link>
+        <NavLink  className={({isActive})=>isActive?'text-blue-500 font-medium text-lg border-b-2':"font-medium text-lg"} to='/' >Home</NavLink>
+        <NavLink className={({isActive})=>isActive?'text-blue-500 font-medium text-lg border-b-2':"font-medium text-lg"} to='/watchlist' >WatchList</NavLink>
+        <NavLink className={({isActive})=>isActive?'text-blue-500 font-medium text-lg border-b-2':"font-medium text-lg"} to='/login' >Login</NavLink>
         {props.mode==='light'?<p className='font-medium text-2xl' onClick={()=>props.handleMode()}><IoIosMoon /></p>:
         <p className='font-medium text-2xl' onClick={()=>props.handleMode()}><IoIosSunny /></p>}
       </div>
