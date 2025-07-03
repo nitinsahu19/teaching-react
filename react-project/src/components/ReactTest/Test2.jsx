f3e4f9cd0b830910cbd0af42cc6b01df6f76bd65
neetukumawat <kumawatneetu728@gmail.com>
2025-07-03 11:02:05 +0530

    update notes React Hook Form

--- react-project/src/components/ReactTest/Test2.jsx
+++ react-project/src/components/ReactTest/Test2.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 
 const Test2 = () => {
-  const [mood, setMood] = useState("");
-    const [moods, setMoods] = useState([]);
+  const [mood, setMood] = useState(""); // State to store the current mood input
+    const [moods, setMoods] = useState([]); // State to store the list of moods
   
     const handleSubmit = (e) => {
-        e.preventDefault();
-        if (mood) {
-          const moods = JSON.parse(localStorage.getItem("moods")) || [];
-          moods.push(mood);
-          localStorage.setItem("moods", JSON.stringify(moods));
-          setMood("");
+        e.preventDefault();  //form submit hone pr page reload nhi hoga
+        if (mood) {    //check if mood is not empty
+          const moods = JSON.parse(localStorage.getItem("moods")) || [];  //get moods from localStorage or initialize as empty array
+          moods.push(mood);  //add new mood to the array
+          localStorage.setItem("moods", JSON.stringify(moods));  //save updated moods to localStorage
+          setMood(""); //clear the input field
         }
       };
   useEffect(() => {
     const storedMoods = JSON.parse(localStorage.getItem("moods")) || [];
-    setMoods(storedMoods);
+    setMoods(storedMoods); 
   }, []);
 
   return (
