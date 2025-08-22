a0d6a5bcb1abee28ae9652b04d67dd105b962867
sureshjoshi88 <sj3795789@gmail.com>
2025-08-22 15:17:39 +0530

    updating

--- src/component/slice/productSlice.jsx
+++ src/component/slice/productSlice.jsx
@@ -1,26 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
- const productSlice = createSlice({
-    name:"apiproduct",
-    initialState :{
-        product:[],
-        loading:false,
-        error:null,
+const productSlice = createSlice({
+    name: "apiproduct",
+    initialState: {
+        product: [],
+        loading: false,
+        error: null,
     },
-    reducers:{
-        fetchingData : (state,action)=>{
+    reducers: {
+        fetchingData: (state, action) => {
             state.product = action.payload
             state.loading = false
         },
-        loadingData : (state)=>{
-            state.loading  = true
+        loadingData: (state) => {
+            state.loading = true
         },
-        errorData : (state)=>{
+        errorData: (state) => {
             state.error = 'something went wrong'
         }
     }
-
 })
-export const {fetchingData,loadingData,errorData} = productSlice.actions;
-
+export const { fetchingData, loadingData, errorData } = productSlice.actions;
 export default productSlice.reducer
